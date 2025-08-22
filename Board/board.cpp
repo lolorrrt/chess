bd9fba0818e1868e3a4e1cdff376c46e56a6313d
lolorrrt <laurence.schreiter@gmail.com>
2025-08-22 23:40:15 +0200

    findBitBoardOfSpecificPiece function for creating merged Bitboards, extended board class

--- Board/board.cpp
+++ Board/board.cpp
@@ -11,7 +11,7 @@ class Field{
     public:
         Field(int initialCoordinates[2], Piece initialPiece) : piece(initialPiece){
             for (int i = 0; i < 2; i++)
-                initialCoordinates[i] = coordinates[i];            
+                coordinates[i] = initialCoordinates[i];            
         }
 };
 
@@ -24,26 +24,61 @@ enum BitBoardType{
     KING,
     WHITE,
     BLACK,
+    ALLFIELDS
 };
 
 class BitBoard{
     private:
        int64_t bits;
-       BitBoardType bitboardtype;
+
     public:
 
+        int64_t getBits(){
+            return bits;
+        }
+
+        BitBoardType bitboardtype;
+
+        BitBoard() : bits(0), bitboardtype(ALLFIELDS){}
         BitBoard(int64_t initialBits, BitBoardType initialBitBoardType) : bits(initialBits), bitboardtype(initialBitBoardType) {}
 
         void convertToCoordinates(){};
-
         void display(){};
-
 };
 
 class Board {
     private:
-        std::vector<BitBoard> bitboard;
+        BitBoard bitboards[8];
+
     public:
-    Board(std::vector<BitBoard> initialBitboard) : bitboard(initialBitboard) {
-     }
+        Board(BitBoard initialBitboards[8]) {
+            for (int i = 0; i < 8; i++)
+                bitboards[i] = initialBitboards[i];    
+         }
+
+         bool checkBitBoardType(BitBoard bitboard, enum BitBoardType bitboardType){
+            return bitboard.bitboardtype == bitboardType;
+         }
+
+         int findBitBoardIndexOfBitBoardType(enum BitBoardType bitboardType){
+            int index = 0;
+            while (checkBitBoardType(bitboards[index], bitboardType))
+                index++;  
+            return index;
+         }
+         
+         u_int16_t findBitBoardOfSpecificPiece(enum BitBoardType Piece, enum BitBoardType Color){
+            int pieceIndex = findBitBoardIndexOfBitBoardType(Piece);
+            int colorIndex = findBitBoardIndexOfBitBoardType(Color);
+            return bitboards[pieceIndex].getBits() & bitboards[colorIndex].getBits();
+         }
+
+         void listOfBlackPawns(){
+
+         }
 };
+
+//BitBoard Klasse muss in karthesische Koordinaten umrechnen
+//BitBoard muss Field erstellen
+//Field Klasse muss Fields anzeigen können
+//gemergede BitBoards -> eigene Klasse; irgendwas überlegen 
